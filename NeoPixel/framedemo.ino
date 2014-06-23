6c6e70322efad79b89232cd64c3f76fcef4ee632
Marie Huynh <mariethuynh@gmail.com>
2014-06-22 20:32:16 -0700

    Smaller and better!

--- NeoPixel/framedemo.ino
+++ NeoPixel/framedemo.ino
@@ -1,8 +1,10 @@
 /*
 Marie Huynh
-June 20, 2014
+June 22, 2014
 
-This is a frame-based blinky test of NeoPixels driven by an Arduino.
+This is a frame-based blinky test of NeoPixels driven by an Arduino.  It compiles to 3,786 bytes, so should fit on Trinkets now.
+
+TODO: Do I need to manually pull strip.Color() out of the loop or will the compiler do that for me because the return value never changes?
 */
 
 #include <Adafruit_NeoPixel.h>
@@ -12,26 +14,50 @@ This is a frame-based blinky test of NeoPixels driven by an Arduino.
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
-// You can define more colors, just plug in the RGB values
-uint32_t OFF = 0;
-uint32_t RED = strip.Color(255,   0,   0);     // Red
-uint32_t GRE = strip.Color(  0, 255,   0);     // Green
-uint32_t BLU = strip.Color(  0,   0, 255);     // Blue
-uint32_t WHI = strip.Color(127, 127, 127); // White
+// New colors may be added, but need their RGB values defined in the switch below.
+// Adhering to three letters is not necessary, but you'll have to fix the spacing and this is already wide.
+typedef enum Colors{
+  OFF,
+  RED,
+  GRE,
+  BLU,
+  YEL,
+  WHI,
+  WH1,
+  WH2,
+  WH3,
+  TEL,
+  TL5
+};
 
-// Remember to change the dimensions below in the loops if you add more rows/frames.
-//                       Pixel:     1    2    3    4    5    6    7    8    9    10   11   12   13   14   15   16 
-uint32_t pattern[10][N_LEDS] = { { WHI, OFF, OFF, WHI, OFF, OFF, WHI, OFF, OFF, GRE, OFF, OFF, GRE, OFF, OFF, GRE },
-                                 { OFF, OFF, WHI, OFF, WHI, OFF, OFF, WHI, GRE, OFF, OFF, GRE, OFF, OFF, GRE, OFF },
-                                 { OFF, WHI, OFF, OFF, OFF, OFF, WHI, OFF, OFF, OFF, GRE, OFF, OFF, GRE, OFF, OFF },
-                                 { OFF, OFF, WHI, OFF, WHI, OFF, OFF, WHI, OFF, GRE, OFF, OFF, GRE, OFF, OFF, GRE },
-                                 { WHI, OFF, WHI, OFF, WHI, OFF, WHI, OFF, GRE, OFF, OFF, GRE, OFF, OFF, GRE, OFF },
-                                 { OFF, WHI, OFF, WHI, OFF, OFF, OFF, WHI, OFF, OFF, GRE, OFF, OFF, GRE, OFF, OFF },
-                                 { WHI, OFF, OFF, OFF, OFF, OFF, WHI, OFF, OFF, GRE, OFF, OFF, GRE, OFF, OFF, GRE },
-                                 { OFF, OFF, OFF, WHI, OFF, WHI, OFF, WHI, GRE, OFF, OFF, GRE, OFF, OFF, GRE, OFF },
-                                 { OFF, OFF, WHI, OFF, WHI, OFF, WHI, OFF, OFF, OFF, GRE, OFF, OFF, GRE, OFF, OFF },
-                                 { OFF, WHI, OFF, OFF, WHI, OFF, OFF, WHI, OFF, GRE, OFF, OFF, GRE, OFF, OFF, GRE }
-                               };
+// Each row is a frame.  New rows may be added.
+// This takes up much less space than the uint32_t that strip.Color() returns.
+//                    Pixel:     1    2    3    4    5    6    7    8    9    10   11   12   13   14   15   16
+uint8_t pattern[][N_LEDS] = { { WHI, OFF, OFF, WHI, OFF, OFF, GRE, OFF, OFF, RED, OFF, TEL, OFF, OFF, OFF, OFF },
+                              { WHI, OFF, WHI, OFF, WHI, OFF, GRE, OFF, GRE, OFF, OFF, TEL, TEL, OFF, OFF, TEL },
+                              { WH3, BLU, OFF, OFF, OFF, OFF, GRE, OFF, OFF, YEL, GRE, TEL, TEL, TEL, OFF, OFF },
+                              { WH2, OFF, WHI, OFF, WHI, OFF, GRE, OFF, OFF, OFF, OFF, TEL, TEL, TEL, TL5, TEL },
+                              { WH1, OFF, WHI, OFF, OFF, WHI, OFF, RED, BLU, OFF, OFF, TL5, TL5, TL5, OFF, OFF },
+                              { OFF, BLU, OFF, WHI, OFF, OFF, OFF, RED, OFF, OFF, BLU, TEL, OFF, OFF, OFF, OFF },
+                              { OFF, OFF, OFF, OFF, OFF, OFF, OFF, RED, OFF, GRE, OFF, TEL, TEL, OFF, OFF, TEL },
+                              { OFF, OFF, OFF, WHI, OFF, WHI, OFF, RED, RED, OFF, OFF, TL5, TL5, OFF, OFF, OFF },
+                              { WH1, OFF, WHI, OFF, WHI, OFF, GRE, OFF, GRE, OFF, OFF, TEL, TEL, OFF, OFF, OFF },
+                              { WH2, BLU, OFF, WHI, OFF, OFF, GRE, OFF, OFF, OFF, RED, TEL, OFF, OFF, OFF, OFF },
+                              { WH3, OFF, OFF, OFF, OFF, OFF, GRE, OFF, BLU, RED, OFF, TEL, TEL, OFF, OFF, TEL },
+                              { WHI, OFF, OFF, OFF, WHI, OFF, GRE, OFF, OFF, OFF, OFF, TEL, TEL, TEL, OFF, OFF },
+                              { WHI, BLU, OFF, WHI, OFF, OFF, OFF, RED, OFF, OFF, YEL, TEL, TEL, TL5, OFF, TEL },
+                              { WHI, OFF, OFF, OFF, OFF, OFF, OFF, RED, OFF, GRE, OFF, TL5, OFF, OFF, OFF, OFF },
+                              { WH3, BLU, OFF, WHI, WHI, OFF, OFF, RED, RED, OFF, OFF, TEL, TEL, OFF, OFF, TEL },
+                              { WH2, OFF, WHI, OFF, OFF, WHI, OFF, RED, OFF, BLU, GRE, TEL, TEL, TEL, TEL, TEL },
+                              { WH1, BLU, OFF, WHI, OFF, OFF, GRE, OFF, OFF, OFF, OFF, TEL, TEL, TL5, TL5, OFF },
+                              { OFF, OFF, OFF, OFF, OFF, OFF, GRE, OFF, OFF, BLU, GRE, TEL, TEL, TEL, OFF, OFF },
+                              { OFF, OFF, WHI, OFF, WHI, OFF, GRE, OFF, GRE, OFF, OFF, TEL, TEL, TEL, TEL, TEL },
+                              { OFF, BLU, OFF, WHI, OFF, OFF, GRE, OFF, OFF, OFF, RED, TEL, TEL, TL5, TL5, OFF },
+                              { WH1, OFF, OFF, OFF, OFF, OFF, OFF, RED, OFF, GRE, OFF, TEL, TL5, OFF, OFF, TEL },
+                              { WH2, OFF, OFF, OFF, OFF, WHI, OFF, RED, BLU, OFF, OFF, TL5, OFF, OFF, OFF, OFF },
+                              { WH3, OFF, WHI, OFF, WHI, OFF, OFF, RED, OFF, OFF, GRE, TEL, TEL, OFF, OFF, OFF },
+                              { WH1, BLU, OFF, OFF, OFF, WHI, OFF, RED, OFF, RED, OFF, TEL, TL5, OFF, OFF, TEL }
+                            };
 
 void setup() {
   strip.begin();
@@ -47,17 +73,55 @@ void loop() {
   blinky(50);
 }
 
-// wait is how long to hold a frame before moving on to the next one. 
 void blinky(uint8_t wait) {
+  uint32_t color;
   // Go through each of the frames
-  for(uint16_t frame = 0; frame < 10; frame++) {
+  for(uint16_t frame = 0; frame < (sizeof(pattern)/sizeof(*pattern)); frame++) {
     // Set each pixel in the frame
     for(uint16_t i=0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, pattern[frame][i]);
+      switch(pattern[frame][i]){
+        case OFF:
+          color = 0;
+          break;
+        case RED:
+          color = strip.Color(255,   0,   0);
+          break;
+        case GRE:
+          color = strip.Color(  0, 255,   0);
+          break;
+        case BLU:
+          color = strip.Color(  0,   0, 255);
+          break;
+        case YEL:
+          color = strip.Color(255, 255,   0);
+          break;
+        case WHI:
+          color = strip.Color(127, 127, 127);
+          break;
+        case WH1:
+          color = strip.Color( 31,  31,  31);
+          break;
+        case WH2:
+          color = strip.Color( 64,  64,  64);
+          break;
+        case WH3:
+          color = strip.Color( 95,  95,  95);
+          break;
+        case TEL:
+          color = strip.Color(  0, 255, 255);
+          break;
+        case TL5:
+          color = strip.Color(  0,  64,  64);
+          break;
+      }
+
+      strip.setPixelColor(i, color);
     }
 
     // Display the frame
     strip.show();
+
+    // Keep that on for some time before moving on to the next frame.
     delay(wait);
   }
 }
