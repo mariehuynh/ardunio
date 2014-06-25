58860377dcaed89459c7ffdddd928183e0505a04
Marie Huynh <mariethuynh@gmail.com>
2014-06-25 12:16:57 -0700

    simpler test

--- NeoPixel/framedemo.ino
+++ NeoPixel/framedemo.ino
@@ -44,7 +44,7 @@ void loop() {
   rainbowCycle(20);
   theaterChaseRainbow(50);*/
 
-  //blinky(20);
+  blinky(20);
 
 }
 
@@ -155,34 +155,34 @@ void blinky(uint8_t wait) {
   // This takes up much less space than the uint32_t that strip.Color() returns.
   //                Pixel:     1    2    3    4    5    6    7    8    9    10   11   12   13   14   15   16
   int pattern[][16] = { { WHI, OFF, OFF, WHI, OFF, OFF, GRE, OFF, OFF, RED, OFF, TEL, OFF, OFF, OFF, OFF },
-                            { WHI, OFF, WHI, OFF, WHI, OFF, GRE, OFF, GRE, OFF, OFF, TEL, TEL, OFF, OFF, TEL },
-                            { WH3, BLU, OFF, OFF, OFF, OFF, GRE, OFF, OFF, YEL, GRE, TEL, TEL, TEL, OFF, OFF },
-                            { WH2, OFF, WHI, OFF, WHI, OFF, GRE, OFF, OFF, OFF, OFF, TEL, TEL, TEL, TL5, TEL },
-                            { WH1, OFF, WHI, OFF, OFF, WHI, OFF, RED, BLU, OFF, OFF, TL5, TL5, TL5, OFF, OFF },
-                            { OFF, BLU, OFF, WHI, OFF, OFF, OFF, RED, OFF, OFF, BLU, TEL, OFF, OFF, OFF, OFF },
-                            { OFF, OFF, OFF, OFF, OFF, OFF, OFF, RED, OFF, GRE, OFF, TEL, TEL, OFF, OFF, TEL },
-                            { OFF, OFF, OFF, WHI, OFF, WHI, OFF, RED, RED, OFF, OFF, TL5, TL5, OFF, OFF, OFF },
-                            { WH1, OFF, WHI, OFF, WHI, OFF, GRE, OFF, GRE, OFF, OFF, TEL, TEL, OFF, OFF, OFF },
-                            { WH2, BLU, OFF, WHI, OFF, OFF, GRE, OFF, OFF, OFF, RED, TEL, OFF, OFF, OFF, OFF },
-                            { WH3, OFF, OFF, OFF, OFF, OFF, GRE, OFF, BLU, RED, OFF, TEL, TEL, OFF, OFF, TEL },
-                            { WHI, OFF, OFF, OFF, WHI, OFF, GRE, OFF, OFF, OFF, OFF, TEL, TEL, TEL, OFF, OFF },
-                            { WHI, BLU, OFF, WHI, OFF, OFF, OFF, RED, OFF, OFF, YEL, TEL, TEL, TL5, OFF, TEL },
-                            { WHI, OFF, OFF, OFF, OFF, OFF, OFF, RED, OFF, GRE, OFF, TL5, OFF, OFF, OFF, OFF },
-                            { WH3, BLU, OFF, WHI, WHI, OFF, OFF, RED, RED, OFF, OFF, TEL, TEL, OFF, OFF, TEL },
-                            { WH2, OFF, WHI, OFF, OFF, WHI, OFF, RED, OFF, BLU, GRE, TEL, TEL, TEL, TEL, TEL },
-                            { WH1, BLU, OFF, WHI, OFF, OFF, GRE, OFF, OFF, OFF, OFF, TEL, TEL, TL5, TL5, OFF },
-                            { OFF, OFF, OFF, OFF, OFF, OFF, GRE, OFF, OFF, BLU, GRE, TEL, TEL, TEL, OFF, OFF },
-                            { OFF, OFF, WHI, OFF, WHI, OFF, GRE, OFF, GRE, OFF, OFF, TEL, TEL, TEL, TEL, TEL },
-                            { OFF, BLU, OFF, WHI, OFF, OFF, GRE, OFF, OFF, OFF, RED, TEL, TEL, TL5, TL5, OFF },
-                            { WH1, OFF, OFF, OFF, OFF, OFF, OFF, RED, OFF, GRE, OFF, TEL, TL5, OFF, OFF, TEL },
-                            { WH2, OFF, OFF, OFF, OFF, WHI, OFF, RED, BLU, OFF, OFF, TL5, OFF, OFF, OFF, OFF },
-                            { WH3, OFF, WHI, OFF, WHI, OFF, OFF, RED, OFF, OFF, GRE, TEL, TEL, OFF, OFF, OFF },
+                            // { WHI, OFF, WHI, OFF, WHI, OFF, GRE, OFF, GRE, OFF, OFF, TEL, TEL, OFF, OFF, TEL },
+                            // { WH3, BLU, OFF, OFF, OFF, OFF, GRE, OFF, OFF, YEL, GRE, TEL, TEL, TEL, OFF, OFF },
+                            // { WH2, OFF, WHI, OFF, WHI, OFF, GRE, OFF, OFF, OFF, OFF, TEL, TEL, TEL, TL5, TEL },
+                            // { WH1, OFF, WHI, OFF, OFF, WHI, OFF, RED, BLU, OFF, OFF, TL5, TL5, TL5, OFF, OFF },
+                            // { OFF, BLU, OFF, WHI, OFF, OFF, OFF, RED, OFF, OFF, BLU, TEL, OFF, OFF, OFF, OFF },
+                            // { OFF, OFF, OFF, OFF, OFF, OFF, OFF, RED, OFF, GRE, OFF, TEL, TEL, OFF, OFF, TEL },
+                            // { OFF, OFF, OFF, WHI, OFF, WHI, OFF, RED, RED, OFF, OFF, TL5, TL5, OFF, OFF, OFF },
+                            // { WH1, OFF, WHI, OFF, WHI, OFF, GRE, OFF, GRE, OFF, OFF, TEL, TEL, OFF, OFF, OFF },
+                            // { WH2, BLU, OFF, WHI, OFF, OFF, GRE, OFF, OFF, OFF, RED, TEL, OFF, OFF, OFF, OFF },
+                            // { WH3, OFF, OFF, OFF, OFF, OFF, GRE, OFF, BLU, RED, OFF, TEL, TEL, OFF, OFF, TEL },
+                            // { WHI, OFF, OFF, OFF, WHI, OFF, GRE, OFF, OFF, OFF, OFF, TEL, TEL, TEL, OFF, OFF },
+                            // { WHI, BLU, OFF, WHI, OFF, OFF, OFF, RED, OFF, OFF, YEL, TEL, TEL, TL5, OFF, TEL },
+                            // { WHI, OFF, OFF, OFF, OFF, OFF, OFF, RED, OFF, GRE, OFF, TL5, OFF, OFF, OFF, OFF },
+                            // { WH3, BLU, OFF, WHI, WHI, OFF, OFF, RED, RED, OFF, OFF, TEL, TEL, OFF, OFF, TEL },
+                            // { WH2, OFF, WHI, OFF, OFF, WHI, OFF, RED, OFF, BLU, GRE, TEL, TEL, TEL, TEL, TEL },
+                            // { WH1, BLU, OFF, WHI, OFF, OFF, GRE, OFF, OFF, OFF, OFF, TEL, TEL, TL5, TL5, OFF },
+                            // { OFF, OFF, OFF, OFF, OFF, OFF, GRE, OFF, OFF, BLU, GRE, TEL, TEL, TEL, OFF, OFF },
+                            // { OFF, OFF, WHI, OFF, WHI, OFF, GRE, OFF, GRE, OFF, OFF, TEL, TEL, TEL, TEL, TEL },
+                            // { OFF, BLU, OFF, WHI, OFF, OFF, GRE, OFF, OFF, OFF, RED, TEL, TEL, TL5, TL5, OFF },
+                            // { WH1, OFF, OFF, OFF, OFF, OFF, OFF, RED, OFF, GRE, OFF, TEL, TL5, OFF, OFF, TEL },
+                            // { WH2, OFF, OFF, OFF, OFF, WHI, OFF, RED, BLU, OFF, OFF, TL5, OFF, OFF, OFF, OFF },
+                            // { WH3, OFF, WHI, OFF, WHI, OFF, OFF, RED, OFF, OFF, GRE, TEL, TEL, OFF, OFF, OFF },
                             { WH1, BLU, OFF, OFF, OFF, WHI, OFF, RED, OFF, RED, OFF, TEL, TL5, OFF, OFF, TEL }
                           };
 
   uint32_t color;
   // Go through each of the frames
-  for(int frame = 0; frame < 24; frame++) {
+  for(int frame = 0; frame < 2; frame++) {
     // Set each pixel in the frame
     for(int i=0; i < strip.numPixels(); i++) {
       color = strip.Color(255,   0,   0); // default red for debugging
